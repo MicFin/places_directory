37e2c545027bc5fa048121043c303d4c2fa7d2a5
Michael Finneran <mrfinneran@gmail.com>
2016-04-01 18:19:35 -0400

    Added Raty for Rating.

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,8 @@ class Place < ActiveRecord::Base
   validates_presence_of :user_id 
 
   belongs_to :user
-  
   geocoded_by :address 
   after_validation :geocode 
+
+  has_many :reviews, dependent: :destroy
 end
